70bdf66d67ebab7c541d5b8eae5b344a38c2f842
hyperion4040 <hyperion1987@wp.pl>
2017-04-20 17:46:59 +0200

    Initial commit

---
+++ Zadanie-1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Zadanie_1
+{
+    class K1
+    {
+        public int A = 13;
+    }
+
+
+    class K2
+    {
+        public int X = 11;
+        public K1 K;
+        public K2()
+        {
+            X = -1;
+            K = new K1();
+            K.A = -7;
+        }
+        public K2(int x, K1 k)
+        {
+            X = x;
+            K = k;
+        }
+
+
+        public static void Main(string[] args)
+        {
+            K2 x = new K2 { X = 3, K = new K1 { A = 5 } };
+            K1 k1 = new K1() { A = 3 };
+            K2 y = new K2(14, new K1()) { X = -1 };
+            Console.WriteLine("x.K.A = {0}, x.X = {1}", x.K.A, x.X);
+            Console.WriteLine("k1.A  = {0}", k1.A);
+            Console.WriteLine("y.K.A = {0}, y.X = {1}", y.K.A, y.X);
+            //Console.ReadLine();
+        }
+    }
+}
---
+++ Zadanie-2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Zadanie_2
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
