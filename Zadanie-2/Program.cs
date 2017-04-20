85923e1d237fd06b2fc5e2dd8caaa8057fe2d3bc
hyperion4040 <hyperion1987@wp.pl>
2017-04-20 21:29:46 +0200

    Added Readme file for Zadanie-2

--- Zadanie-2/Program.cs
+++ Zadanie-2/Program.cs
@@ -3,10 +3,43 @@ using System.Collections.Generic;
 
 namespace Zadanie_2
 {
-    internal class Program
+    class A
     {
-        public static void Main(string[] args)
+        protected A(int i)
         {
+            Console.WriteLine("A: {0}", i);
+        }
+    }
+
+    class B : A
+    {
+        public B(int i) : base(2 * (i + 1))
+        {
+            Console.WriteLine("B: {0}", i);
+        }
+
+        public B() : base(3)
+        {
+            Console.WriteLine("B: {0}", 2.99);
+        }
+    }
+
+    class C : B
+    {
+        C() : base(3)
+        {
+            Console.WriteLine("C: {0}", 6);
+        }
+
+        C(int i) : base(2 * i)
+        {
+            Console.WriteLine("C: {0}", i + 2);
+        }
+
+        static void Main(string[] args)
+        {
+            C x = new C(3);
+            //Console.ReadLine();
         }
     }
 }
