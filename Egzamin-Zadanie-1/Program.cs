7ce962594a0d6c5f26ffe4d7038dbc7454a36274
hyperion4040 <hyperion1987@wp.pl>
2017-04-22 20:22:06 +0200

    added readme in Zadanie-4

---
+++ Egzamin-Zadanie-1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Zadanie_1
+{
+    delegate int Delegacja(int i);
+    class A{
+        public event Delegacja SieDzieje;
+        public int i = 2;
+        public void DoRoboty(int i)
+        {
+            if (SieDzieje != null)
+                this.i += SieDzieje(i);
+            else this.i = i;
+        }
+    }
+    class Test{
+        static void Main(string[] args){
+            A a = new A();
+            a.DoRoboty(2);
+            Console.WriteLine("{0}", a.i);
+            Delegacja raz = x => { Console.WriteLine("jeden"); return x - 1; };
+            a.SieDzieje += new Delegacja(raz);
+            a.DoRoboty(a.i);
+            Console.WriteLine("{0}", a.i);
+            a.SieDzieje += new Delegacja(delegate(int x)
+                { Console.WriteLine("dwa {0}", x * a.i); return 1 + x; });
+            a.DoRoboty(a.i);
+            Console.WriteLine("{0}", a.i);
+            a.SieDzieje += new Delegacja(raz);
+            a.DoRoboty(a.i);
+            Console.WriteLine("{0}", a.i);
+        }
+    }
+}
