d2c4e8363c6b75e06557f0914e32d8fc373c22a4
hyperion4040 <hyperion1987@wp.pl>
2017-04-25 21:27:03 +0200

    Added two new class in library Zadania and Console application in KontoUI

---
+++ Zadania/Osoba.cs
@@ -0,0 +1,26 @@
+﻿namespace Zadania
+{
+    public class Osoba
+    {
+        private string imie;
+        private string nazwisko;
+
+        public string Imie
+        {
+            get { return imie; }
+            set { imie = value; }
+        }
+
+        public string Nazwisko
+        {
+            get { return nazwisko; }
+            set { nazwisko = value; }
+        }
+
+        public Osoba(string imie, string nazwisko)
+        {
+            this.imie = imie;
+            this.nazwisko = nazwisko;
+        }
+    }
+}
