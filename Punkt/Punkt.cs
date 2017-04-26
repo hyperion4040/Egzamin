617c99c65a54500d415add05c0030e9920aa418b
hyperion4040 <hyperion1987@wp.pl>
2017-04-26 22:55:12 +0200

    Added two new class in library Zadania and Console application in KontoUI

---
+++ Punkt/Punkt.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Punkt
+{
+    public class Punkt
+    {
+        public double x = 0, y = 0;
+
+        public void wypisz()
+        {
+            Console.WriteLine("({0}, {1})",x,y);
+        }
+
+        public double odległośćOdPoczątkuUkładu(int x, int y)
+        {
+            return Math.Sqrt(Math.Pow(x - this.x,2) + Math.Pow(y - this.y,2));
+        }
+
+
+    }
+}
